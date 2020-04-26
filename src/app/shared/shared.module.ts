364fec1c2bf1b26f242e39867ceacd10567aae5a
jtpoltorak <jtpoltorak@gmail.com>
2020-04-26 10:27:27 -0400

    Setting up FireBase hooks...

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { AuthService } from './auth.service';
+
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule
-  ],
-  providers: [
-    AuthService
   ]
 })
-export class CoreModule { }
+export class SharedModule { }
